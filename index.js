a2d4c6ffc7cdd50c3c75c4cffff0fb8fca93834d
FarhanZizz <iamfarhanaziz@gmail.com>
2022-11-25 22:00:42 +0530

    added brand based product api

--- index.js
+++ index.js
@@ -11,15 +11,18 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d0hszsm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
-
 
 async function run() {
-    try { }
+    try {
+        const productCollections = client.db('mobile-resell-wizards').collection('products')
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { category: id };
+            const result = await productCollections.find(query).toArray()
+            res.send(result)
+        })
+    }
     finally { }
 }
 
